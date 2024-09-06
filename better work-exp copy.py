150499db61b8689f4fd76f7bc796c88b299ff302
AndrewB2Spirit <ab2spirit@outlook.com>
2024-09-06 16:18:54 +0100

    More code changes (#4)

--- better work-exp copy.py
+++ better work-exp copy.py
@@ -4,6 +4,7 @@ import json
 from dateutil import parser
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
+import random
 
 # Functioms
 def extract_data(row, latcol):
@@ -19,42 +20,40 @@ def extract_data(row, latcol):
 def extract_list(listoflist):
     time = []
     sum = []
-    listoflist.sort()
+    # listoflist.sort()
     for item_in_listoflist in listoflist:
         time.append(item_in_listoflist[0])
         sum.append(item_in_listoflist[1])
     return time, sum
 
 # Main code
-listoflist = []
-listoflist2 = []
+apilists = {}
 latcol = 0
 
+# Read data
 with open("logs/analytics100.csv") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            col_count = 0
-            for col in row:
-                if col == "latency_info":
-                    latcol = col_count
-                else:
-                    col_count += 1
-        else:
-            if row[3] == "wait100ms":
-                listoflist.append(extract_data(row, latcol))
-            else:
-                listoflist2.append(extract_data(row, latcol))
-        line_count += 1
+    labels, *data_rows = csv.reader(csv_file, delimiter=",")
+    for idx, label in enumerate(labels):
+        if label == "latency_info":
+            latcol = idx
+    
+    for row in data_rows:
+        apiname = row[3]
+        if apiname not in apilists:
+            apilists[apiname] = []
+        apilists[apiname].append(extract_data(row, latcol))
 
-time1, sum1 = extract_list(listoflist)
-time3, sum5 = extract_list(listoflist2)
+# print(apilists)
 
+# Plot data
 plt.style.use("_mpl-gallery")
 fig, ax = plt.subplots()
 myFmt = DateFormatter("%H:%M:%S\n%d/%m/%Y")
+colourlist = ["c", "r", "m", "b"]
 ax.xaxis.set_major_formatter(myFmt)
-ax.plot(time1, sum1, "x-c", time3, sum5, "x-m", linewidth = 3)
+for apiname in apilists:
+    timedata, sumdata = extract_list(apilists[apiname])
+    linecolour = random.choice(colourlist)
+    ax.plot(timedata, sumdata, "x-" + linecolour, linewidth = 3)
 
 plt.show()
