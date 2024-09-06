855125ff553c0d2ccb14102328eaeb28d7b81707
Andrii Berezovyi <ab2spirit@outlook.com>
2024-09-06 16:15:27 +0100

    Allow arbitrary number of APIs

--- examples/generate-data.py
+++ examples/generate-data.py
@@ -6,27 +6,47 @@ import random
 import json
 import os
 
+# Function
+
+def create_data(start_date, end_date, min, max, min_steps, steps, range_num):
+    fake = Faker()
+    apitype = ['wait100ms', 'wait200ms', "wait300ms", "wait400ms"]
+    data = []
+    for i in range(range_num):
+        row = {}
+        row['@timestamp'] = fake.date_time_between(start_date=start_date, end_date=end_date,tzinfo=tzlocal()).isoformat(timespec='microseconds')[:-6]+'000Z'
+        row['api_name'] = random.choice(apitype)
+        latency_data = []
+        for j in range(random.randint(min_steps, steps)):
+            step = {}
+            step['started'] = random.randint(min, max)
+            latency_data.append(step) 
+        row['latency_info'] = json.dumps(latency_data)
+        data.append(row)
+    return data
+
+
 # Get the field names
 with open("logs/analytics.csv") as original_file:
     fields, *_ = csv.reader(original_file, delimiter=",")
 
 # Generate data
-start_date = datetime.date(year=2024, month=8, day=1)
-apitype = ['wait100ms', 'wait200ms']
-fake = Faker()
+
+
 data = []
 
-for i in range(100):
-    row = {}
-    row['@timestamp'] = fake.date_time_between(start_date=start_date, end_date='+30d',tzinfo=tzlocal()).isoformat(timespec='microseconds')[:-6]+'000Z'
-    row['api_name'] = random.choice(apitype)
-    latency_data = []
-    for j in range(random.randint(1, 10)):
-        step = {}
-        step['started'] = random.randint(0, 100)
-        latency_data.append(step)
-    row['latency_info'] = json.dumps(latency_data)
-    data.append(row)
+start_date = datetime.date(year=2024, month=8, day=1)
+end_date = datetime.date(year=2024, month=8, day=6)
+data = data + create_data(start_date, end_date, 1, 5, 1, 3, 100)
+
+start_date = datetime.date(year=2024, month=8, day=6)
+end_date = datetime.date(year=2024, month=8, day=8)
+data = data + create_data(start_date, end_date, 11, 15, 4, 9, 40)
+
+start_date = datetime.date(year=2024, month=8, day=8)
+end_date = datetime.date(year=2024, month=8, day=13)
+data = data + create_data(start_date, end_date, 1, 5, 1, 3, 100)
+
 
 data.sort(key=lambda x: x['@timestamp'])
 
